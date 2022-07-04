2fbfd08a6c5ed48a704afeac46e47b5960826422
kamiy2743 <kamiy2743@gmail.com>
2022-07-04 11:04:27 +0900

    APIで書籍情報取得はできた ただレスポンスがjsonじゃない これは APITaskの問題

---
+++ Sample/book_info_api_sample/js/main.js
@@ -0,0 +1,35 @@
+import { APITask } from "../../api_task/js/api-task.js";
+
+const APPLICATION_ID = "1082011979317631304";
+const ACCESS_URL = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?";
+
+/**
+ * 書籍情報をAPIで取得
+ * @param {string | number} isbn ISBNコード13桁 ハイフン削除とかはこの関数でやるから前処理の必要なし
+ * @return {string} JSON形式の書籍情報
+ * @see https://webservice.rakuten.co.jp/documentation/books-book-search
+ */
+async function get_book_info_async(isbn) {
+    const isbn_formatted = isbn.toString().replace("-", "").replace(/\s+/g, "");
+
+    if (isbn_formatted.length != 13) {
+        throw "ISBNコードが13桁ではありません";
+    }
+
+    const params = {
+        format: "json",
+        applicationId: APPLICATION_ID,
+        isbn: isbn_formatted,
+    };
+
+    let requestURL = ACCESS_URL;
+    for (const [key, value] of Object.entries(params)) {
+        requestURL += `&${key}=${value}`;
+    }
+
+    const response = await APITask.get(requestURL);
+    console.log(response);
+}
+
+// いいコード悪いコードのISBN
+get_book_info_async("978-4297127831");
